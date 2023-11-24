5aebdf23529d8eba4f71cc5b0efcd29e880731f6
Xavitheforce <javier.miguelez.yague@gmail.com>
2023-11-24 16:44:37 +0100

    refactorizacion

---
+++ spaghetti.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+def suma(num1, num2):
+    return num1 + num2
+
+def resta(num1, num2):
+    return num1 - num2
+
+def multiplicacion(num1, num2):
+    return num1 * num2
+
+def division(num1, num2):
+    if num2 != 0:
+        return num1 / num2
+    else:
+        print("No se puede dividir entre cero.")
+
